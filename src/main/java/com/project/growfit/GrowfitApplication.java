baa42ee78884deae61d43e8f71b71e14c879576a
Kim Kyeongho <sgn07124@gmail.com>
2025-02-28 16:29:36 +0900

    initial commit

---
+++ src/main/java/com/project/growfit/GrowfitApplication.java
@@ -0,0 +1,13 @@
+package com.project.growfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/growfit/GrowfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.growfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
