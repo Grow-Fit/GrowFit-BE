5156911bffe1a1d352f8c44c84d696e76d3827e8
haennni <ggowater112@naver.com>
2025-04-01 20:38:27 +0900

    feat: WebClient 설정 추가
    -HttpClient에 타임아웃(responseTimeout, CONNECT_TIMEOUT_MILLIS) 설정을 하였습니다.

--- src/main/java/com/project/growfit/global/config/WebClientConfig.java
+++ src/main/java/com/project/growfit/global/config/WebClientConfig.java
@@ -1,11 +1,14 @@
 package com.project.growfit.global.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 
 @Configuration
@@ -14,6 +17,11 @@ public class WebClientConfig {
     public WebClient foodWebClient() {
 
         return WebClient.builder()
+                .clientConnector(
+                        new ReactorClientHttpConnector(
+                                HttpClient.create()
+                                        .responseTimeout(Duration.ofSeconds(60))
+                                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)))
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(configurer -> configurer
                                 .defaultCodecs()
