e1f9472e869d6a9f31c0523fedc9e52c021ff8de
haennni <ggowater112@naver.com>
2025-05-30 06:15:14 +0900

    feat: 식단 관련 Repository 인터페이스 정의
     식단 관련 Repository 인터페이스를 정의하였습니다.

--- src/main/java/com/project/growfit/domain/Diet/repository/DietRepository.java
+++ src/main/java/com/project/growfit/domain/Diet/repository/DietRepository.java
@@ -1,8 +1,6 @@
 package com.project.growfit.domain.Diet.repository;
 
-import com.project.growfit.domain.Diet.entity.DailyDiet;
 import com.project.growfit.domain.Diet.entity.Diet;
-import com.project.growfit.domain.Diet.entity.MealType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,9 +12,5 @@ import java.util.Optional;
 public interface DietRepository extends JpaRepository<Diet, Long> {
     @Query("SELECT d FROM Diet d JOIN FETCH d.foodList WHERE d.id = :dietId")
     Optional<Diet> findDietWithFoodList(@Param("dietId") Long dietId);
-
-    Optional<Diet> findByMealType(MealType mealType);
-
-    boolean existsByDailyDietAndMealType(DailyDiet dailyDiet, MealType mealType);
 
 }
--- src/main/java/com/project/growfit/domain/Diet/repository/FoodRepository.java
+++ src/main/java/com/project/growfit/domain/Diet/repository/FoodRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
-
-
 }
