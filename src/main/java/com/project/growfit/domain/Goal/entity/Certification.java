4639dc279622b520317aa84c426b1abdf1301c99
haennni <ggowater112@naver.com>
2025-06-24 16:03:55 +0900

    feat: 목표 관련 엔티티 메서드 추가

--- src/main/java/com/project/growfit/domain/Goal/entity/Certification.java
+++ src/main/java/com/project/growfit/domain/Goal/entity/Certification.java
@@ -1,5 +1,7 @@
 package com.project.growfit.domain.Goal.entity;
 
+import com.project.growfit.global.exception.BusinessException;
+import com.project.growfit.global.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,7 +11,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,4 +40,27 @@ public class Certification {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "goal_id", nullable = false)
     private Goal goal;
+
+    public static Certification create(String imageUrl, Goal goal) {
+        LocalDate now = LocalDate.now();
+        checkDateValidity(goal.getCertificationList(), now);
+
+        Certification certification = new Certification();
+        certification.imageUrl = imageUrl;
+        certification.certifiedAt = LocalDateTime.now();
+        certification.goal = goal;
+        return certification;
+    }
+
+    public void assignToGoal(Goal goal) {
+
+        this.goal = goal;
+    }
+
+    private static void checkDateValidity(List<Certification> certifications, LocalDate date) {
+        boolean exists = certifications.stream()
+                .anyMatch(cert -> cert.getCertifiedAt().toLocalDate().isEqual(date));
+        if (exists) throw new BusinessException(ErrorCode.ALREADY_CERTIFIED_TODAY);
+    }
+
 }
